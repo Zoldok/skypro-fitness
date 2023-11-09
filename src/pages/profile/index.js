81d2ecc7a5aef1bc9b14829cd0f8bf45e5aa26bd
Zoldok <max_kyrtimov@mail.ru>
2023-11-09 21:56:26 +0300

    go я создал

---
+++ src/pages/profile/index.js
@@ -0,0 +1,15 @@
+
+
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  color: white;
+`;
+
+export default function Profile() {
+  return (
+    <StyledSection>
+      <p>Hello World Motherfucker</p>
+    </StyledSection>
+  );
+}
