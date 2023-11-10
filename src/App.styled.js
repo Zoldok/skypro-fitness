9eff3f915fbf0c8400c3bfcd35a9c0d7676a05ec
Zoldok <max_kyrtimov@mail.ru>
2023-11-10 21:01:10 +0300

    fix2

--- src/App.styled.js
+++ src/App.styled.js
@@ -79,7 +79,7 @@ export const MainApp = styled.div`
   }
   margin: 0;
   padding: 0;
-  background-color: #181818;
+  // background-color: #181818;
   // height: 100vh;
   // max-width: 1240px;
   // display: flex;
@@ -90,33 +90,33 @@ export const MainApp = styled.div`
 
 export const AppHeader = styled.header``
 
-export const Wrapper = styled.div`
-  width: 100%;
-  min-height: 100%;
-  overflow: hidden;
-  background-color: #383838;
-`
+// export const Wrapper = styled.div`
+//   width: 100%;
+//   min-height: 100%;
+//   overflow: hidden;
+//   background-color: #383838;
+// `
 
-export const Container = styled.div`
-  max-width: 1920px;
-  height: 100vh;
-  margin: 0 auto;
-  position: relative;
-  background-color: #181818;
-`
+// export const Container = styled.div`
+//   max-width: 1920px;
+//   height: 100vh;
+//   margin: 0 auto;
+//   position: relative;
+//   background-color: #181818;
+// `
 
-export const Main = styled.main`
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-wrap: wrap;
-  flex-wrap: nowrap;
-  // flex-wrap: wrap;
-  -webkit-box-pack: justify;
-  -ms-flex-pack: justify;
-  justify-content: space-between;
-`
-export const Footer = styled.footer``
+// export const Main = styled.main`
+//   -webkit-box-flex: 1;
+//   -ms-flex: 1 1 auto;
+//   flex: 1 1 auto;
+//   display: -webkit-box;
+//   display: -ms-flexbox;
+//   display: flex;
+//   -ms-flex-wrap: wrap;
+//   flex-wrap: nowrap;
+//   // flex-wrap: wrap;
+//   -webkit-box-pack: justify;
+//   -ms-flex-pack: justify;
+//   justify-content: space-between;
+// `
+// export const Footer = styled.footer``
