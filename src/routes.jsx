743d83ab2cc0230a4eb508a7c83dafa73b45ca2e
*твое <*>
2023-11-13 23:21:42 +0500

    изменил страницу авторизации и логина

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,15 @@
-
 import { Routes, Route } from 'react-router-dom'
+
+import Main from './pages/Main/Main'
 import Profile from './pages/profile'
+import AuthPage from './pages/Authpage/AuthPage'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/AuthPage" element={<AuthPage />} />
     </Routes>
   )
 }
