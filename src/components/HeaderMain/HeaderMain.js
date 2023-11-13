231174d9504a3e491edd7a65031ce366907e9f6b
Olya-Myalo <olyamyalo@mail.ru>
2023-11-13 21:27:51 +0300

    работа с апи

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom"
 import * as S from './HeaderMain.styled'
+import { useGetAllCoursesQuery } from "../../Service/Service"
 
 function HeaderMain() {
+  const { data } = useGetAllCoursesQuery();
+  console.log(data);
+
   return (
     <S.HeaderMain>
       <S.LogoMain src="../../../img/LogoMain.svg" alt="logo" />
