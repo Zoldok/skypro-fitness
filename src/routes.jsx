11636b092a3c7039fb0f2e9009279501e8a1a042
*твое <*>
2023-11-13 21:12:31 +0500

    routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import AuthPage from './components/Authpage/AuthPage'
-import CorrectAuth from './components/CorrectAuth/Correct'
-import Header from './components/Header/Header'
 import Profile from './pages/profile'
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/AuthPage" element={<AuthPage />} />
-      <Route path="/CorrectAuth" element={<CorrectAuth />} />
-      <Route path="/" element={<Header />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   )
