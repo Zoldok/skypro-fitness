5944b4b253cd6ceb52b9f873430acb48079494ad
Olya-Myalo <olyamyalo@mail.ru>
2023-11-29 21:33:43 +0300

    main

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,7 +4,7 @@ import Profile from './pages/profile'
 import Acquaint from './pages/acquaint/acquaint'
 import AuthPage from './pages/Authpage/AuthPage'
 import ProtectedRoute from './components/ProtectedRoute/protected-route'
-import Main from './pages/main/Main'
+import Main from './pages/Main/Main'
 import TrainingPage from './pages/training/training'
 
 export default function AppRoutes() {
